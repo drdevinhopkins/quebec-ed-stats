2808efbb4db7a8996de6913f34d9879757dd1d85
drdevinhopkins <devinhopkins@gmail.com>
2020-01-25 18:42:49 -0500

    connected to dropbox

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 # from datetime import datetime, timedelta
 # import requests
 # from datetime import date
-# import pytz
+# import pytzg
 # import urllib
 # import urllib.parse
 # import json
@@ -14,15 +14,58 @@ import pandas as pd
 # from urllib.request import urlopen
 # import numpy as np
 # import pydeck as pdk
+import os
+import argparse
+import contextlib
+import datetime
+import os
+import six
+import sys
+import time
+import unicodedata
+import dropbox
+from dotenv import load_dotenv
+
+
+def upload(dbx, fullname, folder, subfolder, name, overwrite=False):
+    """Upload a file.
+    Return the request response, or None in case of error.
+    """
+    path = '/%s/%s/%s' % (folder, subfolder.replace(os.path.sep, '/'), name)
+    while '//' in path:
+        path = path.replace('//', '/')
+    mode = (dropbox.files.WriteMode.overwrite
+            if overwrite
+            else dropbox.files.WriteMode.add)
+    mtime = os.path.getmtime(fullname)
+    with open(fullname, 'rb') as f:
+        data = f.read()
+    # with stopwatch('upload %d bytes' % len(data)):
+    try:
+        res = dbx.files_upload(
+            data, path, mode,
+            client_modified=datetime.datetime(*time.gmtime(mtime)[:6]),
+            mute=True)
+    except dropbox.exceptions.ApiError as err:
+        print('*** API error', err)
+        return None
+    print('uploaded as', res.name.encode('utf8'))
+    return res
 
 
 def main():
+    dropbox_api_key = os.environ['DROPBOX_ED_API']
+    dbx = dropbox.Dropbox(dropbox_api_key)
+    dbx.users_get_current_account()
+
+    data_url = 'https://www.dropbox.com/s/folba7dxjgo5pwx/data.csv?dl=1'
+
     st.title('Hourly ED Data')
 
     my_placeholder = st.empty()
     my_placeholder.text("Loading data...")
 
-    old_data = pd.read_csv('data.csv')
+    old_data = pd.read_csv(data_url)
     old_data['Mise_a_jour'] = pd.to_datetime(old_data['Mise_a_jour'])
     old_data["Heure_de_l'extraction_(image)"] = pd.to_datetime(
         old_data["Heure_de_l'extraction_(image)"])
@@ -57,6 +100,8 @@ def main():
     st.write(concat_data)
 
     concat_data.to_csv('data.csv', index=False)
+
+    upload(dbx, 'data.csv', '', '', 'newdata.csv')
 
 
 if __name__ == "__main__":
