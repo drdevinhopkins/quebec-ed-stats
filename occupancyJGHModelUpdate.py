331c31f20d77ca31939c743c6b9ba3caaa14e974
drdevinhopkins <devinhopkins@gmail.com>
2020-02-01 21:48:30 -0500

    a

--- occupancyJGHModelUpdate.py
+++ occupancyJGHModelUpdate.py
@@ -33,71 +33,22 @@ def main():
     print('connected to dropbox')
 
     df = pd.read_csv(
-        'https://www.dropbox.com/s/keafvwlkboedkdm/jghDailyVisits.csv?dl=1')
+        'https://www.dropbox.com/s/fqsdx1ovqsljwqa/jghOccupancy.csv?dl=1')
     df.ds = pd.to_datetime(df.ds)
-    print('jgh daily visits loaded')
+    print('jgh occupancy loaded')
 
     stat_days_df = pd.read_csv(
         'https://www.dropbox.com/s/hj3byufwtypi8d3/statdays.csv?dl=1')
 
-    ramq_stat_days = stat_days_df['RAMQ']
-    ramq_stat_days = ramq_stat_days.dropna()
-    jgh_stat_days = stat_days_df['JGH']
-    jgh_stat_days = jgh_stat_days.dropna()
-    ramq_stat_days = pd.DataFrame({
-        'holiday': 'ramq',
-        'ds': ramq_stat_days.to_list(),
-        'lower_window': 0,
-        'upper_window': 1,
-    })
-    jgh_stat_days = pd.DataFrame({
-        'holiday': 'jgh',
-        'ds': jgh_stat_days.to_list(),
-        'lower_window': 0,
-        'upper_window': 1,
-    })
-    holidays = pd.concat((ramq_stat_days, jgh_stat_days))
-    print('stat days prepared')
-
-    # Get all Montreal Canadiens games in our time frame (2012-present) from the NHL's REST API
-    hockey_df = pd.read_csv(
-        'https://www.dropbox.com/s/g00ih64ndksshwr/montrealHockey.csv?dl=1')
-    hockey_df.dateTime = pd.to_datetime(hockey_df.dateTime)
-
-    # Separate the games by Home or Away
-    home_hockey_df = hockey_df[hockey_df['homeTeam'] == 'Montreal Canadiens']
-
-    away_hockey_df = hockey_df[hockey_df['awayTeam'] == "Montreal Canadiens"]
-
-    # Format them as Prophet expects
-    home_hockey = pd.DataFrame({
-        'holiday': 'homeHockey',
-        'ds': pd.to_datetime(home_hockey_df['dateTime']),
-        'lower_window': 0,
-        'upper_window': 1,
-    })
-
-    away_hockey = pd.DataFrame({
-        'holiday': 'awayHockey',
-        'ds': pd.to_datetime(away_hockey_df['dateTime']),
-        'lower_window': 0,
-        'upper_window': 1,
-    })
-
-    # Add the hockey games to our stat day data
-    holidays = pd.concat(
-        (home_hockey, away_hockey, ramq_stat_days, jgh_stat_days))
-    print('hockey games added')
-
     # Load our weather data from Dropbox
     weather_df = pd.read_csv(
-        'https://www.dropbox.com/s/688u8aw6k0eqzqb/montrealDailyWeather.csv?dl=1')
+        'https://www.dropbox.com/s/omkh9t1lrg5k914/montrealHourlyWeather.csv?dl=1')
     weather_df['ds'] = pd.to_datetime(weather_df['ds'])
     print('old weather added')
 
     # Fetch whicher days are missing, and 2 days into the future (weather forecast), which is the maximum the WWO API let's us access through the this API
     # The Data is saved as a csv file called 'Montreal'
-    frequency = 24
+    frequency = 1
     start_date = (weather_df.ds.max()-timedelta(days=2)
                   ).date().strftime("%d-%b-%Y").upper()
     end_date = (datetime.now(pytz.utc)).astimezone(
@@ -123,11 +74,12 @@ def main():
 
     # Concatenate the old weather data with the missing weather data
     final_weather_df = pd.concat([weather_df, missing_weather_df])
+    final_weather_df = final_weather_df.drop_duplicates()
 
-    final_weather_df.to_csv('montrealDailyWeather.csv', index=False)
+    final_weather_df.to_csv('montrealHourlyWeather.csv', index=False)
 
-    upload(dbx, 'montrealDailyWeather.csv', '', '',
-           'montrealDailyWeather.csv', overwrite=True)
+    upload(dbx, 'montrealHourlyWeather.csv', '', '',
+           'montrealHourlyWeather.csv', overwrite=True)
 
     regressors = final_weather_df.columns.to_list()
     regressors.remove('ds')
@@ -137,7 +89,7 @@ def main():
 
     print('starting to build model')
     # Instantiate our model with our stat day and hockey variables saved in the 'holidays' dataframe
-    m = Prophet(holidays=holidays, seasonality_mode='multiplicative',
+    m = Prophet(seasonality_mode='multiplicative',
                 changepoint_prior_scale=0.5, changepoint_range=0.85)
     # Add Prophet's built-in holidays for Canada
     m.add_country_holidays(country_name='CA')
@@ -151,13 +103,13 @@ def main():
     print('finished fitting model')
 
     import pickle
-    pkl_path = "jgh-prophet-daily.pkl"
+    pkl_path = "jgh-prophet-occupancy.pkl"
     with open(pkl_path, "wb") as f:
         # Pickle the 'Prophet' model using the highest protocol available.
         pickle.dump(m, f)
 
-    upload(dbx, 'jgh-prophet-daily.pkl', '', '',
-           'jgh-prophet-daily.pkl', overwrite=True)
+    upload(dbx, 'jgh-prophet-occupancy.pkl', '', '',
+           'jgh-prophet-occupancy.pkl', overwrite=True)
 
 
 if __name__ == "__main__":
